efe5be864baf9257f04fb550ce3cd0cb38cafeb7
istoleherdress <104830707+istoleherdress@users.noreply.github.com>
2025-09-20 11:42:17 +0300

    Add files via upload

---
+++ exec.lua
@@ -0,0 +1,26 @@
+local Players = game:GetService("Players")
+
+local Automoderation = {}
+
+Automoderation.BannedUsers = {
+	{0, "Debugging account! You cannot access this experience."}
+}
+
+Automoderation.CheckPlayerStatus = function(Player)
+	for _, user in ipairs(Automoderation.BannedUsers) do
+		if Player.UserId == user[1] then
+			Player:Kick(user[2])
+			return
+		end
+	end
+end
+
+Automoderation.AttachAutomoderation = function()
+	for _, Player in pairs(Players:GetPlayers()) do
+		Automoderation.CheckPlayerStatus(Player)
+	end
+	
+	Players.PlayerAdded:Connect(function(Player)
+		Automoderation.CheckPlayerStatus(Player)
+	end)
+end
